35f9e33948be51d5bf55069c943aa06f9190be23
Mister Maximus <92948927+wiseman-timelord@users.noreply.github.com>
2023-12-19 21:52:32 +0000

    Upgraded a lot.

--- main.ps1
+++ main.ps1
@@ -1,56 +1,57 @@
-# Set up global variables
-$Script:currentFolder = $null
-$Script:completedFolder = $null
+# Initialization
+$Host.UI.RawUI.ForegroundColor = 'Yellow'
+$Host.UI.RawUI.BackgroundColor = 'Black'
 
-function Start-Script {
-    Write-Host "Starting WGet-GGUF-Hug..."
-    Start-Sleep -Seconds 1
+# Global variables
+$global:downloadFolder = ".\temporary"
+$global:completedFolder = ".\Completed"
+
+# Create necessary folders
+function Create-Folder($path) {
+    $null = New-Item -ItemType Directory -Force -Path $path
 }
 
+# Start script message
+function Start-Script {
+    Write-Host "Starting WGet-LLM-Hug..."
+    Start-Sleep -Seconds 2
+}
+
+# Initialize folders
 function Initialize-Folders {
-    # Create necessary folders and set initial folders
-    $Script:currentFolder = Join-Path $PSScriptRoot "Downloads"
-    $Script:completedFolder = Join-Path $PSScriptRoot "Completed"
-    $null = New-Item -ItemType Directory -Force -Path $Script:currentFolder
-    $null = New-Item -ItemType Directory -Force -Path $Script:completedFolder
+    Create-Folder $global:downloadFolder
+    Create-Folder $global:completedFolder
 }
 
 function Extract-Filename {
-    param (
-        [string]$url
-    )
+    param ([string]$url)
 
-    # Find the index of ".gguf"
+    # Handle both .gguf and .gptq file formats
     $ggufIndex = $url.IndexOf(".gguf")
+    $gptqIndex = $url.IndexOf(".gptq")
 
-    if ($ggufIndex -ge 0) {
-        # Work back to the previous "%"
-        $percentIndex = $url.LastIndexOf("%", $ggufIndex)
-
-        if ($percentIndex -ge 0) {
-            # Count two characters after the "%"
-            $startIndex = $percentIndex + 3
-            $filename = $url.Substring($startIndex, $ggufIndex - $startIndex + 5)
-			Write-Host "`nFilename Extracted: $filename"
-			Start-Sleep -Seconds 2
-            return $filename
+    foreach ($index in @($ggufIndex, $gptqIndex)) {
+        if ($index -ge 0) {
+            $percentIndex = $url.LastIndexOf("%", $index)
+            if ($percentIndex -ge 0) {
+                $startIndex = $percentIndex + 3
+                $filename = $url.Substring($startIndex, $index - $startIndex + 5)
+                Write-Host "`nFilename Extracted: $filename" -ForegroundColor Green
+                Start-Sleep -Seconds 2
+                return $filename
+            }
         }
     }
 
-    throw "Unable to extract filename from the URL."
-}
-
-function Show-Menu {
-    Clear-Host
-    Write-Host "`n                     -= WGet-GGUF-Hug =-`n"
-    Write-Host "                1) Enter Download URL"
-    Write-Host "                2) Scan Downloads Folder"
-    Write-Host "                3) Scan Completed Folder"
-    Write-Host "                0) Exit Program`n"
+    # Instead of using 'throw', we use Write-Host to display the error message in red
+    Write-Host "Unable to extract filename from the URL." -ForegroundColor Red
+    # Optionally, you can throw an exception after displaying the message
+    throw "Filename extraction failed"
 }
 
 function Download {
-    $url = Read-Host "Enter URL"
+    Write-Host ""
+	$url = Read-Host "Enter URL"
 
     try {
         $filename = Extract-Filename -url $url
@@ -58,56 +59,74 @@ function Download {
         Write-Host "`nFilename Extracted: $filename"
 
         $completedFilename = Join-Path $Script:completedFolder $filename
-        $downloadPath = Join-Path $Script:currentFolder $filename
+        $downloadPath = Join-Path $Script:downloadFolder $filename
 
-        if (Test-Path $downloadPath) {
-            Write-Host "File '$filename' already exists in $Script:currentFolder..."
-            Start-Sleep -Seconds 2
-        } elseif (Test-Path $completedFilename) {
+        # Diagnostics: Print out the paths
+        Write-Host "Download Path: $downloadPath"
+        Write-Host "Completed File Path: $completedFilename"
+
+        if (Test-Path $completedFilename) {
             Write-Host "Download of '$filename' already completed!"
             Start-Sleep -Seconds 2
         } else {
-            Write-Host "Downloading '$filename'..."
+            if (Test-Path $downloadPath) {
+                Write-Host "File '$filename' already exists in ${Script:downloadFolder}, starting new download..."
+            } else {
+                Write-Host "Downloading '$filename'..."
+            }
 
-            # Construct the full path for the download file
-            $downloadPath = Join-Path $Script:currentFolder $filename
+            $downloadCommand = ".\libraries\wget.exe"
+            $arguments = "--no-check-certificate", "-O", "`"$($downloadPath)`"", "$($url)"
 
-            # Use Start-Process to run wget, specifying the output file
-            $downloadCommand = "wget.exe"
-            $arguments = "-O", "`"$($downloadPath)`"", "$($url)"
+            # Diagnostics: Print out the command and arguments
+            Write-Host "Command to execute: $downloadCommand"
+            Write-Host "Arguments: $($arguments -join ' ')"
 
-            # Capture the output of the wget command
-            $output = Start-Process -FilePath $downloadCommand -ArgumentList $arguments -Wait -NoNewWindow -PassThru | Out-String
-
-            Write-Host "wget Output: $($output)"
+            Start-Process -FilePath $downloadCommand -ArgumentList $arguments -Wait -NoNewWindow -PassThru | Out-String
 
             if ($LASTEXITCODE -eq 0) {
-                Write-Host "Download of '$filename' completed successfully."
+                Write-Host "Download of '$filename' success." -ForegroundColor Green
                 Move-Item -Path $downloadPath -Destination $Script:completedFolder -Force
-                Write-Host "Move to $Script:completedFolder completed successfully."
+                Write-Host "Moved to ${Script:completedFolder} successfully." 
             } else {
-                Write-Host "Download of '$filename' failed."
+                Write-Host "Download of '$filename' failed." -ForegroundColor Red
             }
         }
     } catch {
         Write-Host "Error: $_"
-        Start-Sleep -Seconds 30
+        Start-Sleep -Seconds 2
     }
 }
 
+function Scan-Folders {
+    Write-Host "Scanning Folders..."
 
-
-function Scan-Folder {
-    Write-Host "Scanning $Script:currentFolder Folder..."
-    $files = Get-ChildItem -Path $Script:currentFolder -Filter *.gguf
-
-    if ($files.Count -eq 0) {
-        Write-Host "No files in $Script:currentFolder..."
-    } else {
-        $files | ForEach-Object { Write-Host "$($_.Name)" }
+    function Scan-Directory($path, $folderName) {
+        Write-Host "`n${folderName}:"
+        $files = Get-ChildItem -Path $path -Include "*.gguf", "*.gptq" -Recurse
+        if ($files.Count -eq 0) { Write-Host "No files in ${folderName}..." }
+        else { $files | ForEach-Object { Write-Host "$($_.Name)" } }
     }
 
+    Scan-Directory $Script:downloadFolder "temporary"
+    Scan-Directory $Script:completedFolder "Completed"
     Start-Sleep -Seconds 2
+}
+
+function Empty-Temp {
+    Write-Host "`nEmptying Temporary Folder..."
+    Get-ChildItem -Path $Script:downloadFolder -Recurse | Remove-Item -Force
+    Write-Host "...Temporary Folder Emptied.`n"
+	Start-Sleep -Seconds 2
+}
+
+function Show-Menu {
+    Clear-Host
+    Write-Host "`n`n`n`n`n`n                     -= WGet-LLM-Hug =-`n"
+    Write-Host "                      1) Enter URL"
+    Write-Host "                      2) Scan Folders"
+    Write-Host "                      3) Empty Temp"
+    Write-Host "                      0) Exit Program`n"
 }
 
 function Main {
@@ -119,11 +138,11 @@ function Main {
         $choice = Read-Host "Enter your choice"
 
         switch ($choice) {
-            0 { return }
-            1 { Download }
-            2 { $Script:currentFolder = Join-Path $PSScriptRoot "Downloads"; Scan-Folder }
-            3 { $Script:currentFolder = Join-Path $PSScriptRoot "Completed"; Scan-Folder }
-            default { Write-Host "Invalid choice. Try again." }
+            "0" { break }
+            "1" { Download }
+            "2" { Scan-Folders }
+            "3" { Empty-Temp }
+            default { Write-Host "Invalid choice. Try again." -ForegroundColor Red }
         }
     } while ($true)
 }
